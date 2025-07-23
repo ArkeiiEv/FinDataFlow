aa8b6a6c1f80d72512b3b3781ae3afa330ac9258
Alex Kirei <a_kirei@display>
2025-07-23 10:19:52 +0300

    feat: private data is loaded from .env
    - Added opportunity to start script without docker

--- src/data_extractor/data_extractor.py
+++ src/data_extractor/data_extractor.py
@@ -3,10 +3,13 @@ import csv
 import os
 import logging
 from datetime import datetime
+from dotenv import load_dotenv
+
+load_dotenv()
 
 API_URL = "https://www.alphavantage.co/query"
-API_KEY = "U8JFI4JGIG1L23F1"
-SYMBOL = "IBM"
+API_KEY = os.getenv('API_KEY')
+SYMBOL = os.getenv('SYMBOL')
 
 
 def extract_data():
@@ -21,9 +24,15 @@ def extract_data():
         response = requests.get(API_URL, params=params)
         response.raise_for_status()
 
+        base_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+        raw_data_dir = os.path.join(base_dir, "data", "raw")
+
         today = datetime.now().strftime("%Y%m%d")
-        os.makedirs("/data/raw", exist_ok=True)
-        file_path = f"/data/raw/stock_prices_{today}.csv"
+
+        os.makedirs(raw_data_dir, exist_ok=True)
+        file_path = os.path.join(raw_data_dir, f"stock_prices_{today}.csv")
+        #os.makedirs("/data/raw", exist_ok=True)
+        #file_path = f"/data/raw/stock_prices_{today}.csv"
 
         with open(file_path, 'w') as f:
             f.write(response.text)
